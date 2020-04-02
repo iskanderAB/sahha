cceccb2ba774638d7de49125076deda902f22004
Mou3in02 <mou3in02@gmail.com>
2020-04-02 18:30:25 +0100

    init-add

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,21 +3,27 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/api/add/user")
+     * @Route("/api/add/user",methods={"POST"})
      */
-    public function addUser(Request $request) : Response{
+    public function addUser(Request $request,SerializerInterface $serializer,UserPasswordEncoderInterface $passwordEncoder) : Response{
 
         $response = new Response();
         $data = $request->getContent();
-        dd($data);
+        $user = $serializer->deserialize($data,User::class,"json");
+        $password = $user->getPassword();
+        $user->setPassword($passwordEncoder->encodePassword($user,$password));
+        dd($user);
 
         return $response;
     }
