a1a5d0e3d9a02763bf00f0afc454cde1cba67566
Mou3in02 <mou3in02@gmail.com>
2020-04-02 16:47:04 +0100

    init

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,8 +3,22 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class UserController
+class UserController extends AbstractController
 {
+    /**
+     * @Route("/api/add/user")
+     */
+    public function addUser(Request $request) : Response{
 
+        $response = new Response();
+        $data = $request->getContent();
+        dd($data);
+
+        return $response;
+    }
 }
