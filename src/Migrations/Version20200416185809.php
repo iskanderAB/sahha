7b77a8c892997f21ecfbcaedb8f418e0adf28d4d
Mou3in02 <mou3in02@gmail.com>
2020-04-16 20:02:00 +0100

    folder

--- src/Migrations/Version20200416185809.php
+++ src/Migrations/Version20200416185809.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200415043318 extends AbstractMigration
+final class Version20200416185809 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,9 @@ final class Version20200415043318 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE answer CHANGE from_doctor_id from_doctor_id INT DEFAULT NULL, CHANGE survey_id survey_id INT DEFAULT NULL, CHANGE content content VARCHAR(1000) DEFAULT NULL');
+        $this->addSql('CREATE TABLE answer (id INT AUTO_INCREMENT NOT NULL, from_doctor_id INT DEFAULT NULL, survey_id INT DEFAULT NULL, content VARCHAR(1000) DEFAULT NULL, INDEX IDX_DADD4A2597686EAD (from_doctor_id), UNIQUE INDEX UNIQ_DADD4A25B3FE509D (survey_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A2597686EAD FOREIGN KEY (from_doctor_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A25B3FE509D FOREIGN KEY (survey_id) REFERENCES survey (id)');
         $this->addSql('ALTER TABLE question CHANGE survey_id survey_id INT DEFAULT NULL, CHANGE title title VARCHAR(255) DEFAULT NULL, CHANGE response response VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE survey CHANGE created_by_id created_by_id INT DEFAULT NULL, CHANGE created_at created_at VARCHAR(255) DEFAULT NULL, CHANGE feedback feedback VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL, CHANGE first_name first_name VARCHAR(100) DEFAULT NULL, CHANGE last_name last_name VARCHAR(100) DEFAULT NULL, CHANGE birthday birthday VARCHAR(50) DEFAULT NULL, CHANGE gouvernorate gouvernorate VARCHAR(50) DEFAULT NULL, CHANGE gender gender VARCHAR(1) DEFAULT NULL, CHANGE phone_number phone_number INT DEFAULT NULL, CHANGE address address VARCHAR(255) DEFAULT NULL');
@@ -33,7 +35,7 @@ final class Version20200415043318 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE answer CHANGE from_doctor_id from_doctor_id INT DEFAULT NULL, CHANGE survey_id survey_id INT DEFAULT NULL, CHANGE content content VARCHAR(1000) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP TABLE answer');
         $this->addSql('ALTER TABLE question CHANGE survey_id survey_id INT DEFAULT NULL, CHANGE title title VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE response response VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE survey CHANGE created_by_id created_by_id INT DEFAULT NULL, CHANGE created_at created_at VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE feedback feedback VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`, CHANGE first_name first_name VARCHAR(100) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE last_name last_name VARCHAR(100) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE birthday birthday VARCHAR(50) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE gouvernorate gouvernorate VARCHAR(50) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE gender gender VARCHAR(1) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE phone_number phone_number INT DEFAULT NULL, CHANGE address address VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
